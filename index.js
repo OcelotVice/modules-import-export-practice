30081956bea41b5d8d46a736a76474514b5f06a9
OcelotVice <iamgabrielvice@gmail.com>
2022-07-22 18:08:07 +0300

    first-commit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 
 // Your code here
+const {sayHelloTo} = require('./send-messages/say-hello-to');
+const {msg1,msg2,msg3} = require('./messages/index');
+const {giveMessageToMrsPotato} = require('./send-messages/give-message-to-mrs-potato')
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
